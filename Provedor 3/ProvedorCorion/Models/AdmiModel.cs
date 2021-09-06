082606cc8a0b6079e176a8b5491fae43177b397d
Jorge <87153508+Jorge81L@users.noreply.github.com>
2021-09-06 12:46:39 -0600

    Add files via upload
    se agrego el proyecto de proveedor 3 corion con las clases modelo

---
+++ Provedor 3/ProvedorCorion/Models/AdmiModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ProvedorCorion.Models
+{
+    public class AdmiModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Pass { get; set; }
+        public AdmiModel()
+        {
+
+        }
+    }
+}
