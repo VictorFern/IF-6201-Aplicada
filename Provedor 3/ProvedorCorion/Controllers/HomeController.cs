b0e73675067ecc00aca2041678899c2467d65095
Jorge <jorgemadrigalsolano81@gmail.com>
2021-10-01 11:10:48 -0600

    se agrego una busqueda por nombre

--- Provedor 3/ProvedorCorion/Controllers/HomeController.cs
+++ Provedor 3/ProvedorCorion/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ProvedorCorion.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,10 +14,12 @@ namespace ProvedorCorion.Controllers
 {
     public class HomeController : Controller
     {
+        public IConfiguration Configuration { get; set; }
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            Configuration = configuration;
             _logger = logger;
         }
 
