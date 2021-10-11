bf2c6bb63f253706da89b1f40ed157ae612fb74a
Victor <victorfern48@gmail.com>
2021-10-11 14:56:55 -0600

    Se corrige un error con un pull

---
+++ Pagina Principal/IF6201__TomeYLleve/IF6201_TomeYLleve/wwwroot/js/site.js
@@ -0,0 +1,53 @@
+﻿function AgregarCarrito(idP, idU, cant) {
+    var parametros = {
+        "idP": idP, "idU": idU, "cant": cant
+    };
+
+    $.ajax(
+        {
+            data: parametros,
+            url: '/Cliente/AgregarCarrito',
+            type: 'post',
+            beforeSend: function () {     
+                $("#resultado").html("Procesando")
+            },
+            success: function (response) {
+                $("#resultado").html(response)
+            }
+        }
+    );
+}
+
+function disminuir(c,p,t,ca) {
+    inicio = document.getElementById(c).value;
+    if (inicio > 1) {
+        var cantidad = document.getElementById(c).value = --inicio;
+        document.getElementById(ca).value = cantidad;
+        var precio = document.getElementById(p).value;
+        var total = cantidad * precio;
+        document.getElementById(t).value = total;
+        var subtotalTemp = document.getElementById("subtotal").value;
+
+        var subtotal = parseInt(subtotalTemp) - parseInt(precio);
+        document.getElementById("subtotal").value = subtotal;
+    }
+}
+
+function aumentar(c, p, t, ca,s) {
+    cantStock = document.getElementById(s).value;
+    inicio = document.getElementById(c).value;
+    var cantidad = document.getElementById(c).value = ++inicio;
+    if (cantidad <= cantStock) {
+        document.getElementById(ca).value = cantidad;
+        var precio = document.getElementById(p).value;
+        var total = cantidad * precio;
+        document.getElementById(t).value = total;
+        var subtotalTemp = document.getElementById("subtotal").value;
+
+        var subtotal = parseInt(subtotalTemp) + parseInt(precio);
+        document.getElementById("subtotal").value = subtotal;
+    } else if (cantidad > cantStock) {
+        alert("La cantidad deseada no está disponible");
+        document.getElementById(c).value = --inicio
+    }
+}
