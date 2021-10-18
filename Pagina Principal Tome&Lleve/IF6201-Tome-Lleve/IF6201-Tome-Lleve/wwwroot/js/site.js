9f0528e75e681ef0d1e30085551acce54dd0d1c6
Victor <victorfern48@gmail.com>
2021-10-18 10:51:27 -0600

    Se añade las funciones de disminuir y aumentar cantidad y la de eliminar carrito

--- Pagina Principal Tome&Lleve/IF6201-Tome-Lleve/IF6201-Tome-Lleve/wwwroot/js/site.js
+++ Pagina Principal Tome&Lleve/IF6201-Tome-Lleve/IF6201-Tome-Lleve/wwwroot/js/site.js
@@ -18,7 +18,7 @@
     );
 }
 
-function disminuir(c,p,t,ca) {
+function disminuir(c, p, t, ca, idP, idU, asc) {
     inicio = document.getElementById(c).value;
     if (inicio > 1) {
         var cantidad = document.getElementById(c).value = --inicio;
@@ -30,10 +30,28 @@ function disminuir(c,p,t,ca) {
 
         var subtotal = parseInt(subtotalTemp) - parseInt(precio);
         document.getElementById("subtotal").value = subtotal;
+
+        var parametros = {
+            "idP": idP, "idU": idU, "cant": asc
+        };
+
+        $.ajax(
+            {
+                data: parametros,
+                url: '/Cliente/UpdateCarrito',
+                type: 'post',
+                beforeSend: function () {
+                    $("#mensaje").html("Procesando, \n\ espere por favor...");
+                },
+                success: function (response) {
+
+                }
+            }
+        );
     }
 }
 
-function aumentar(c, p, t, ca,s) {
+function aumentar(c, p, t, ca, s, idP, idU, asc) {
     cantStock = document.getElementById(s).value;
     inicio = document.getElementById(c).value;
     var cantidad = document.getElementById(c).value = ++inicio;
@@ -46,8 +64,48 @@ function aumentar(c, p, t, ca,s) {
 
         var subtotal = parseInt(subtotalTemp) + parseInt(precio);
         document.getElementById("subtotal").value = subtotal;
+
+        var parametros = {
+            "idP": idP, "idU": idU, "cant": asc
+        };
+
+        $.ajax(
+            {
+                data: parametros,
+                url: '/Cliente/UpdateCarrito',
+                type: 'post',
+                beforeSend: function () {
+                    $("#mensaje").html("Procesando, \n\ espere por favor...");
+                },
+                success: function (response) {
+
+                }
+            }
+        );
     } else if (cantidad > cantStock) {
         alert("La cantidad deseada no está disponible");
         document.getElementById(c).value = --inicio
     }
+}
+
+function EliminarCarrito(idP, idU, idF, idT) {
+    var parametros = {
+        "idP": idP, "idU": idU
+    };
+
+    $.ajax(
+        {
+            data: parametros,
+            url: '/Cliente/EliminarCarrito',
+            type: 'post',
+            success: function (response) {
+                alert("Articulo eliminado");
+                $("#" + idF).remove();
+
+                var subtotalTemp = document.getElementById("subtotal").value;
+                var subtotal = parseFloat(subtotalTemp) - parseFloat(idT);
+                document.getElementById("subtotal").value = subtotal;
+            }
+        }
+    );
 }
