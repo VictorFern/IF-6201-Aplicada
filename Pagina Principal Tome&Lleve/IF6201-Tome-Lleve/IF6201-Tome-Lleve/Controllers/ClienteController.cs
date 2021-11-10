a508628a9de574e174533669ddff8eb1f58e7465
Victor <victorfern48@gmail.com>
2021-11-09 22:38:47 -0600

    Se añade las funciones de cerrar session, comprar y comprar el carrito con sus procedure

--- Pagina Principal Tome&Lleve/IF6201-Tome-Lleve/IF6201-Tome-Lleve/Controllers/ClienteController.cs
+++ Pagina Principal Tome&Lleve/IF6201-Tome-Lleve/IF6201-Tome-Lleve/Controllers/ClienteController.cs
@@ -214,7 +214,6 @@ namespace IF6201_TomeYLleve.Controllers
                                                                            , Int32.Parse(productoReader["CANTIDAD"].ToString())
                                                                            , productoReader["FOTO"].ToString()
                                                                            ,precioTemp
-                                                                           ,subTotalTemp
                                                                            , Int32.Parse(productoReader["STOCK"].ToString()));
                             lista.Add(productoTemp);
                         }
@@ -262,6 +261,148 @@ namespace IF6201_TomeYLleve.Controllers
                 }
             }
             return new JsonResult("");
+        }
+        public ActionResult cerrarSesion()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("iniciarSesion", "Usuario");
+        }
+        public IActionResult comprar(int id)
+        {
+            List<object> lista = new List<object>();
+            ComprarProducto comprarProducto = new ComprarProducto();
+            CategoriaModel categoriaModel = new CategoriaModel();
+            ProveedorModel proveedorModel = new ProveedorModel();
+            if (ModelState.IsValid)
+            {
+                string connectioString = Configuration["ConnectionStrings:DB_Connection"];
+                using (SqlConnection connection = new SqlConnection(connectioString))
+                {
+                    string sqlQuery = $"sp_detalleProducto @param_ID='{id}'";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.CommandType = System.Data.CommandType.Text;
+                        connection.Open();
+                        SqlDataReader productoReader = command.ExecuteReader();
+                        while (productoReader.Read())
+                        {
+
+                            categoriaModel.categoria = productoReader["CATEGORIA"].ToString();
+
+                            proveedorModel.proveedor = productoReader["PROVEEDOR"].ToString();
+                            ProductoModel productoTemp = new ProductoModel(Int32.Parse(productoReader["IDP"].ToString())
+                                                                           , productoReader["NOMBREP"].ToString()
+                                                                           , productoReader["MARCA"].ToString()
+                                                                           , productoReader["DESCRIPCION"].ToString()
+                                                                           , Int32.Parse(productoReader["PRECIO"].ToString())
+                                                                           , productoReader["DIMENSION"].ToString()
+                                                                           , productoReader["OTRASCARACTERISTICAS"].ToString()
+                                                                           , productoReader["FOTO"].ToString()
+                                                                           , Int32.Parse(productoReader["CANTIDAD"].ToString())
+                                                                           , categoriaModel
+                                                                           , proveedorModel);
+
+                            comprarProducto.productoModel = productoTemp;
+                        }
+                        connection.Close();
+                    }
+                }
+            }
+            return View(comprarProducto);
+        }
+
+        public IActionResult detalleCompra(ComprarProducto comprar)
+        {
+
+            int monto = comprar.cantidad * comprar.productoModel.precio;
+            comprar.monto = monto;
+            string direccion = comprar.provincia + ", " + comprar.canton + ", " + comprar.detalle;
+            comprar.direccion = direccion;
+            return View(comprar);
+        }
+
+        [HttpPost]
+        public IActionResult pagarProducto(ComprarProducto comprar)
+        {
+            int valor = (int)HttpContext.Session.GetInt32("variableInt");
+
+            if (ModelState.IsValid)
+            {
+                string connectionString = Configuration["ConnectionStrings:DB_Connection"];
+                var connection = new SqlConnection(connectionString);
+                string sqlQuery = $"exec sp_PAGAR @param_PROVINCIA='{comprar.provincia}',@param_CANTON='{comprar.canton}',@param_DETALLE='{comprar.detalle}',@param_TIPO='{comprar.tipo}',@param_NUMERO='{comprar.numero}',@param_CVV='{comprar.codigoSeguridad}',@param_FECHA='{comprar.fechaVencimiento}',@param_CEDULA={valor},@param_IDP='{comprar.productoModel.id}',@param_CANTIDAD='{comprar.cantidad}',@param_MONTO='{comprar.monto}'";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    connection.Open();
+                    command.ExecuteReader();
+                    connection.Close();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult comprarCarrito(ComprarCarrito comprar)
+        {
+            int idU = (int)HttpContext.Session.GetInt32("variableInt");
+            int precioTemp = 0;
+            int subTotalTemp = 0;
+            List<ProductoModel> lista = new List<ProductoModel>();
+            ComprarCarrito comprarCarrito = new ComprarCarrito();
+            if (ModelState.IsValid)
+            {
+                string connectioString = Configuration["ConnectionStrings:DB_Connection"];
+                using (SqlConnection connection = new SqlConnection(connectioString))
+                {
+                    string sqlQuery = $"sp_verCarrito @param_IDU='{idU}'";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.CommandType = System.Data.CommandType.Text;
+                        connection.Open();
+                        SqlDataReader productoReader = command.ExecuteReader();
+
+                        while (productoReader.Read())
+                        {
+                            precioTemp = Int32.Parse(productoReader["CANTIDAD"].ToString()) * Int32.Parse(productoReader["PRECIO"].ToString());
+                            subTotalTemp += precioTemp;
+                            ProductoModel productoTemp = new ProductoModel(Int32.Parse(productoReader["IDP"].ToString())
+                                                                           , productoReader["NOMBREP"].ToString()
+                                                                           , Int32.Parse(productoReader["PRECIO"].ToString())
+                                                                           , Int32.Parse(productoReader["CANTIDAD"].ToString())
+                                                                           , productoReader["FOTO"].ToString()
+                                                                           , precioTemp
+                                                                           , Int32.Parse(productoReader["STOCK"].ToString()));
+
+                            lista.Add(productoTemp);
+                            comprarCarrito.productoModel.Add(productoTemp);
+
+                        }
+                        connection.Close();
+                    }
+                }
+            }
+            comprarCarrito.monto = subTotalTemp;
+            return View(comprarCarrito);
+        }
+        [HttpPost]
+        public IActionResult pagarCarrito(ComprarCarrito comprar)
+        {
+            int valor = (int)HttpContext.Session.GetInt32("variableInt");
+
+            if (ModelState.IsValid)
+            {
+                string connectionString = Configuration["ConnectionStrings:DB_Connection"];
+                var connection = new SqlConnection(connectionString);
+                string sqlQuery = $"exec sp_PAGARCARRITO @param_PROVINCIA='{comprar.provincia}',@param_CANTON='{comprar.canton}',@param_DETALLE='{comprar.detalle}',@param_TIPO='{comprar.tipo}',@param_NUMERO='{comprar.numero}',@param_CVV='{comprar.codigoSeguridad}',@param_FECHA='{comprar.fechaVencimiento}',@param_CEDULA='{valor}'";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    connection.Open();
+                    command.ExecuteReader();
+                    connection.Close();
+                }
+            }
+            return RedirectToAction("verCarrito");
         }
     }
 }
