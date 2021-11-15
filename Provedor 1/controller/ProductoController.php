c9bb9a61d9a3642ccbc8fd69b76ac37e2ed15307
Victor <victorfern48@gmail.com>
2021-11-15 10:22:22 -0600

    Se añade el envio de productos por api

--- Provedor 1/controller/ProductoController.php
+++ Provedor 1/controller/ProductoController.php
@@ -39,7 +39,7 @@ class ProductoController {
             $uploadFileDir = './public/img/';
             $dest_path = $uploadFileDir . $newFileName;
             if (move_uploaded_file($fileTmpPath, $dest_path)) {
-                $cliente->registrar($_POST['Nombre'], $_POST['Descripcion'], $_POST['Precio'], $_POST['Talla'], $_POST['Color'], $_POST['Marca'], $newFileName, $_POST['Categoria'],$_POST['Cantidad']);
+                $cliente->registrar($_POST['Nombre'], $_POST['Descripcion'], $_POST['Precio'], $_POST['Talla'], $_POST['Color'], $_POST['Marca'], $newFileName, $_POST['Categoria'], $_POST['Cantidad']);
                 $this->view->show("insertarProductoView.php", $cliente->mostrar_Categorias());
             }
         }
@@ -93,7 +93,7 @@ class ProductoController {
     public function actualizaProducto() {
         require 'model/ProductoModel.php';
         $cliente = new ProductoModel();
-        $cliente->actualizar($_POST['ID'], $_POST['NOMBRE'], $_POST['DESCRIPCION'], $_POST['PRECIO'], $_POST['TALLA'], $_POST['COLOR'], $_POST['MARCA'], $_POST['Categoria'],$_POST['Cantidad']);
+        $cliente->actualizar($_POST['ID'], $_POST['NOMBRE'], $_POST['DESCRIPCION'], $_POST['PRECIO'], $_POST['TALLA'], $_POST['COLOR'], $_POST['MARCA'], $_POST['Categoria'], $_POST['Cantidad']);
         $data['listado'] = $cliente->ver_Producto($_POST['ID']);
         $this->view->show("mostrarProductoView.php", $data);
     }
@@ -117,24 +117,47 @@ class ProductoController {
 
         $this->view->show("loginView.php");
     }
+
     public function solicitar() {
         require 'model/ProductoModel.php';
         $cliente = new ProductoModel();
         $data['productos'] = $cliente->solicitar_key();
         $this->view->show("loginView.php", null);
     }
-    
+
     public function inicioSeccion() {
         require 'model/ProductoModel.php';
-         $cliente = new ProductoModel();
+        $cliente = new ProductoModel();
         $data['seccion'] = $cliente->inicioSeccion($_POST['clave']);
         $data['listado'] = $cliente->mostrar_Producto();
         $_SESSION['usuario'] = $data['seccion'];
-        $this->view->show("enviarView.php",$data);
+        $this->view->show("enviarView.php", $data);
     }
+
     public function cerrarSesion() {
         session_destroy();
         $this->view->show("loginView.php", null);
+    }
+
+    public function enviarApi() {
+        $apiurl = 'http://apitomeylleve-001-site1.etempurl.com/api/Productos/Registro';
+        $producto = array('id' => 0, 'nombre' => $_POST['nombre'], 'marca' => $_POST['marca'], 'descripcion' => $_POST['descripcion'], 'precio' => $_POST['precio'], 'dimensiones' => $_POST['dimension'], 'otrasCaracteristicas' => $_POST['otras'], 'foto' => $_POST['foto'], 'categoria' => $_POST['categoria'], 'idProveedor' => 1, 'cantidad' => $_POST['cantidad']);
+        
+        $data_array = json_encode($producto);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiurl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_array);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $result = curl_exec($curl);
+        echo "<pre>";
+        print_r(json_decode($result, true));
+        echo "</pre>";
+        curl_close($curl);
+        require 'model/ProductoModel.php';
+        $cliente = new ProductoModel();
+        $data['listado'] = $cliente->mostrar_Producto();
+        $this->view->show("enviarView.php", $data);
     }
 }
 
--- Provedor 1/public/js/funcion.js
+++ Provedor 1/public/js/funcion.js
@@ -1,43 +1,51 @@
-//
-function enviar(marca, nombreP, descripcion, precio, dimension, otras, foto, cantidad, categoria) {
 
-    //alert(JSON.stringify(producto));
-    var url = "http://apitomeylleve-001-site1.etempurl.com/api/Productos/Registro";
-    var request = new XMLHttpRequest();
-
-    request.open("POST", url);
-//    request.setRequestHeader("Content-Type", "application/json");
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status==200) {
-            console.log(request.status);
-            console.log(request.responseText);
+function enviar(id) {
+    var formData = new FormData(document.getElementById(id));
+    alert(JSON.stringify(formData));
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://apitomeylleve-001-site1.etempurl.com/api/Productos/Registro",true);
+      if (xhr.readyState === 4 && xhr.status === 200) {
+            var serverresponse = JSON.parse(xhr.responseText);
+            console.log(serverresponse);
         }
-    };
-    var producto = {"id": 0, "nombre": nombreP, "marca": marca, "descripcion": descripcion, "precio": precio, "dimensiones": dimension, "otrasCaracteristicas": otras, "foto": foto, "categoria": categoria, "idProveedor": 1, "cantidad": cantidad};
-    request.send(JSON.stringify(producto));
+    xhr.send(JSON.stringify(formData));
+    //alert(JSON.stringify(producto));
+//    var url = "http://apitomeylleve-001-site1.etempurl.com/api/Productos/Registro";
+//    var request = new XMLHttpRequest();
+
+//    request.open("POST", url);
+////    request.setRequestHeader("Content-Type", "application/json");
+//    request.onreadystatechange = function () {
+//        if (request.readyState === 4 && request.status == 200) {
+//            console.log(request.status);
+//            console.log(request.responseText);
+//        }
+//    };
+//    var producto = {"id": 0, "nombre": nombreP, "marca": marca, "descripcion": descripcion, "precio": precio, "dimensiones": dimension, "otrasCaracteristicas": otras, "foto": foto, "categoria": categoria, "idProveedor": 1, "cantidad": cantidad};
+//    request.send(JSON.stringify(producto));
 }
 
-//function enviar(marca, nombreP, descripcion, precio, dimension, otras, foto, cantidad, categoria) {
-//    var producto = {"id": 0, "nombre": nombreP, "marca": marca, "descripcion": descripcion, "precio": precio, "dimensiones": dimension, "otrasCaracteristicas": otras, "foto": foto, "categoria": categoria, "idProveedor": 1, "cantidad": cantidad};
-//    alert(JSON.stringify(producto));
-//    $.ajax(
-//            {
-//                url: 'http://apitomeylleve-001-site1.etempurl.com/api/Productos/Registro/',
-//                type: 'post',
-//                data: JSON.stringify(producto),
-//                contentType: false,
-//                cache: false,
-//                processData: false,
-//                dataType: 'json',
-//                beforeSend: function () {
-////                    $("#resultado").html("Procesando, espere");
-//                },
-//                success: function (response) {
-////                    $("#resultado").html(response);
-//                    console.log(response);
-//                    alert(response);
-//                }
-//            }
-//    );
-//
-//}//registrarProductoAjax
+function enviar2(marca, nombreP, descripcion, precio, dimension, otras, foto, cantidad, categoria) {
+    var producto = {"id": 0, "nombre": nombreP, "marca": marca, "descripcion": descripcion, "precio": precio, "dimensiones": dimension, "otrasCaracteristicas": otras, "foto": foto, "categoria": categoria, "idProveedor": 1, "cantidad": cantidad};
+    alert(JSON.stringify(producto));
+    $.ajax(
+            {
+                url: 'http://apitomeylleve-001-site1.etempurl.com/api/Productos/Registro/',
+                type: 'post',
+                data: JSON.stringify(producto),
+                contentType: false,
+                cache: false,
+                processData: false,
+                dataType: 'json',
+                beforeSend: function () {
+//                    $("#resultado").html("Procesando, espere");
+                },
+                success: function (response) {
+//                    $("#resultado").html(response);
+                    console.log(response);
+                    alert(response);
+                }
+            }
+    );
+
+}//registrarProductoAjax
